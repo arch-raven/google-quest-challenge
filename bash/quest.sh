8ca7c871b427056eb91ef8cabbda828a47838d85
arch-raven <adityabk80@gmail.com>
2021-02-23 21:16:49 +0530

    minor

--- bash/quest.sh
+++ bash/quest.sh
@@ -5,9 +5,9 @@ module load cuda
 module load anaconda/3
 source activate torchenv
 cd google-quest-challenge/
-python src/main.py --fold 0 --model_name ccluster
-python src/main.py --fold 1 --model_name ccluster
-python src/main.py --fold 2 --model_name ccluster
-python src/main.py --fold 3 --model_name ccluster
-python src/main.py --fold 4 --model_name ccluster
+python src/train.py --fold 0 --model_name ccluster
+python src/train.py --fold 1 --model_name ccluster
+python src/train.py --fold 2 --model_name ccluster
+python src/train.py --fold 3 --model_name ccluster
+python src/train.py --fold 4 --model_name ccluster
 source deactivate
