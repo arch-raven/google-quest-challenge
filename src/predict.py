37df7d342836d45e68ac9f98e7b7a5f4c6ec7f00
arch-raven <adityabk80@gmail.com>
2021-02-24 06:20:43 +0530

    mod predict.py

--- src/predict.py
+++ src/predict.py
@@ -19,39 +19,41 @@ def predict_and_save(args, model, dataloader):
             batch["token_type_ids"],
             batch["target"],
         )
-        
+
         logits = model.forward(ids_seq, attn_masks, token_type_ids)
         outputs.append(logits)
-    
+
     y_pred = torch.sigmoid(torch.cat(outputs)).to("cpu").detach().numpy()
-    
+
     submission_df = pd.read_csv("../output/sample_submission.csv")
-    submission_df.loc[:,data.target_cols] = y_pred 
-    
+    submission_df.loc[:,data.target_cols] = y_pred
+
     if not args.output_filename.endswith(".csv"):
         args.output_filename = f"{args.output_filename}.csv"
-    
+
     submission_df.to_csv(
         f"output/{args.output_filename}",
-        index=False, 
+        index=False,
     )
     print(f"predictions saved in file {args.output_filename}")
-    
+
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("ckpt_folder", default="models",type=str)
-    parser.add_argument("output_filename", default="none",type=str, help="regex pattern or filename")
+    parser.add_argument("--ckpt_folder", default="models",type=str)
+    parser.add_argument("--output_filename", default="none",type=str, help="regex pattern or filename")
+    parser.add_argument("--maxlen", default=512, type=int)
+    parser.add_argument("--batch_size", default=8, type=int)
     args = parser.parse_args()
-    
+
     data = QuestData(args)
-    model = QuestModel(args)
-    
+    #model = QuestModel(args)
+
     test_dataloader = data.test_dataloader()
-    
+
     for checkpoint in [f for f in os.listdir(args.ckpt_folder) if f.endswith(".ckpt")]:
         args.output_filename = f"{checkpoint[:-5]}.csv"
-        if not os.path.exists(args.output_filename): 
-            model.load_from_checkpoint(checkpoint)
+        if not os.path.exists(args.output_filename):
+            model = QuestModel.load_from_checkpoint(f"models/{checkpoint}")
             predict_and_save(args, model, test_dataloader)
         else:
             print(f"prediction csv file for {checkpoint} already exists")
